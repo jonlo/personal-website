9997f1c967cbf50766eaa48263f2647faadaf753
Jon Lopez de Guereña <jon.lopezdeguerena@tecnalia.com>
2022-10-14 10:05:23 +0200

    experience added

--- src/components/experience.js
+++ src/components/experience.js
@@ -12,14 +12,19 @@ export class Experience extends Component {
 		return (
 			<div className="Experience">
 				<h2>Experience</h2>
-				<ul>
+				<ul className='Experience-list'>
 					{this.props.experience.map((experience, index) => {
 						return <li key={index}>
+							<h3> {experience.position}</h3>
 							<div className='experience-header'>
 								<h4>{experience.company}</h4>
 								<h5><i>{experience.from} - {experience.to} </i></h5>
 							</div>
-							<p> {experience.position}</p>
+							<ul className='Task-list'>
+								{experience.tasks.map((task, index) => {
+									return <li key={index}> {task}</li>;
+								})}
+							</ul>
 						</li>;
 					})}
 				</ul>
