52b683492d9032e77ecc5c786cd3c4a5e2e668bb
Jon Lopez de Guereña <jon.lopezdeguerena@tecnalia.com>
2022-10-18 06:15:07 +0200

    fixed deploy pipeline

--- src/components/skills.js
+++ src/components/skills.js
@@ -6,7 +6,7 @@ export class Skills extends Component {
 	render() {
 		return (
 			<div className="Skills">
-				<h2>Skills</h2>
+				<h2>Technologies I work with</h2>
 				<ul>
 					{this.props.skills.map((skill, index) => {
 						return <li key={index}>{skill.name}</li>;
